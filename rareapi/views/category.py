a45ba91e02c1ad6edcfebad25038b7dcef1aeda7
Kevin Caruso <Kjcaruso12@gmail.com>
2022-05-13 13:35:27 -0500

    Added create function to CategoryView

---
+++ rareapi/views/category.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from rareapi.models import Category
+
+class CategoryView(ViewSet):
+    """Rare posts view"""
+
+    def list(self, request):
+        """Handle GET requests to get all posts
+
+        Returns:
+            Response -- JSON serialized list of posts
+        """
+        try:
+            categories = Category.objects.order_by('label')
+            serializer = CategorySerializer(categories, many=True)
+            return Response(serializer.data)
+        except Category.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized category instance
+        """
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+class CategorySerializer(serializers.ModelSerializer):
+    """JSON serializer for posts
+    """
+
+    class Meta:
+        model = Category
+        fields = ('id', 'label')
+
