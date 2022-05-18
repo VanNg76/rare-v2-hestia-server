287e87c10ff3374f3810c1a3543554c6e03525ad
Van Nguyen <thanh_xer@yahoo.com>
2022-05-18 09:57:35 -0500

    added postCount

--- rareapi/loaddata.sql
+++ rareapi/loaddata.sql
@@ -9,3 +9,12 @@ where id in (5,6,7);
 
 INSERT INTO rareapi_comment (created_on, content, author_id, post_id)
 VALUES ('2020-01-05', 'SQL comment', 2, 1)
+
+insert into rareapi_post_tags (post_id, tag_id)
+values (1,1);
+insert into rareapi_post_tags (post_id, tag_id)
+values (2,3);
+insert into rareapi_post_tags (post_id, tag_id)
+values (3,5);
+insert into rareapi_post_tags (post_id, tag_id)
+values (4,4);
